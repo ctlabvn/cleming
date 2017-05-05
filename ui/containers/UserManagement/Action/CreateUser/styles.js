ae7b853d41c2589e89541f4782f26c282857153c
vjtc0n <vjtc0n12345@gmail.com>
2017-05-05 15:17:23 +0700

    Update create user

--- ui/containers/UserManagement/Action/CreateUser/styles.js
+++ ui/containers/UserManagement/Action/CreateUser/styles.js
@@ -4,8 +4,74 @@
 export default {
     inputField: {
         borderRadius: 5,
-        paddingLeft: 20,
         backgroundColor: '#d9d9d9',
-        color: 'red'
+        height: 50
+    },
+    inputContainer: {
+        marginBottom: 15
+    },
+    inputIcon: {
+        fontSize: 20,
+        color: '#7e7e7e'
+    },
+    listItem:{
+        marginLeft:10,
+        marginRight:10,
+        paddingLeft:10,
+        paddingRight:10,
+        height:40,
+        borderBottomWidth: 0,
+        paddingBottom: 0
+    },
+    right: {
+        flexDirection:'row',
+        justifyContent:'flex-end',
+        flex:1,
+        marginRight: 10
+    },
+    left: {
+        flex: 1,
+        color: '#a2a2a2'
+    },
+    leftAddressTitleText: {
+        fontSize: 15,
+        color: '#7e7e7e',
+        fontWeight: 'bold'
+    },
+    rightAddressTitleText: {
+        fontSize: 15,
+        color: '#00a9d7'
+    },
+    createPasswordButton: {
+        backgroundColor: '#00a9d7',
+        alignSelf: 'center',
+        width: '80%',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    createPasswordButtonText: {
+        color: 'white'
+    },
+    passwordText: {
+        color: '#00a9d7',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    copyText: {
+        color: '#00a9d7',
+        fontSize: 14,
+    },
+    submitButton: {
+        backgroundColor: '#00a9d7',
+        alignSelf: 'center',
+        width: '100%',
+        borderRadius: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    copyIcon: {
+        color: '#00a9d7',
+        marginRight: 5
     }
 }
