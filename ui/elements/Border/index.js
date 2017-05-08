b6282c2b7d87b6ffb480c39495d17a607c41126e
hai.vu <vu.longhai93@gmail.com>
2017-05-08 08:59:42 +0700

    Update border add vertical case

--- ui/elements/Border/index.js
+++ ui/elements/Border/index.js
@@ -3,33 +3,66 @@ import material from '~/theme/variables/material'
 import { View } from 'react-native'
 
 
-export default class extends PureComponent{
+export default class extends PureComponent {
 
-  render(){
-    const {color='red', size=2, padding=2, style} = this.props
-    const num = material.deviceWidth / (size + padding * 2)
-    
-    const borders = []
-    for(let i=0;i<num;i++){
-      borders.push(<View key={i} style={{
+  render() {
+    const { color = 'red', size = 2, padding = 2, orientation = 'horizontal', num=20} = this.props
+    if (orientation == 'horizontal') {
+      const num = material.deviceWidth / (size + padding * 2)
+      const borders = []
+      for (let i = 0; i < num; i++) {
+        borders.push(<View key={i} style={{
           backgroundColor: color,
           width: size,
           height: size,
-          borderRadius:size/2,
-        }}/>  
+          borderRadius: size / 2,
+        }} />
+        )
+      }
+
+      return (
+        <View style={{
+          overflow: 'hidden',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}
+          onLayout={(event) => {
+            console.log('Height', event.nativeEvent.layout.height)
+            console.log('Width', event.nativeEvent.layout.width)
+          }}
+        >
+          {borders}
+        </View>
+      )
+    }else{
+      const borders = []
+      for (let i = 0; i < num; i++) {
+        borders.push(<View key={i} style={{
+          backgroundColor: color,
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+          marginBottom: padding
+        }} />
+        )
+      }
+      return (
+        <View style={{
+          overflow: 'hidden',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+          onLayout={(event) => {
+            console.log('vertical case')
+            console.log('Height', event.nativeEvent.layout.height)
+            console.log('Width', event.nativeEvent.layout.width)
+          }}
+        >
+          {borders}
+        </View>
       )
     }
 
-    return (
-      <View style={{
-        overflow: 'hidden',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        ...style,
-      }}>
-        {borders}
-      </View>
-    )
   }
-} 
+}
 
