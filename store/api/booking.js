79e9622e3ff0ee6c627ee92d2cac5dabcdacf968
hai.vu <vu.longhai93@gmail.com>
2017-05-12 11:20:23 +0700

    Update Place Order

--- store/api/booking.js
+++ store/api/booking.js
@@ -2,9 +2,11 @@ import { apiGet } from '~/store/api/common'
 export default {
     // /booking/list?from_time=1494435600&to_time=1494499066&place_list=1223,2334&status=0&page=1
 
-    list(xsession, place_list, from_time=1320985607, to_time=1510374407, status=0, page = 1) {
+    list(xsession, place_list='', from_time=0, to_time=1510374407, status=0, page = 1) {
         console.log('Go to booking API', place_list)
         console.log('xss booking', xsession)
+        console.log('page', page)
+        console.log('status', status)
         return apiGet('/booking/list', {place_list, from_time, to_time, status, page}, xsession)
     }
 }
---
+++ ui/containers/PlaceOrderList/options.js
@@ -0,0 +1,13 @@
+export default {
+    tabData : [
+        {
+            tabID: 1,
+            text: 'Chờ xác nhận',
+            number: 2
+        },
+        {
+            tabID: 2,
+            text: 'Đã xác nhận'
+        }
+    ]
+}
