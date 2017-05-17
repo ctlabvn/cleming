b1d30c193e759c6bc696f9c9164ca11835802e61
hai.vu <vu.longhai93@gmail.com>
2017-05-17 08:53:28 +0700

    Add dateFilter DeliveryList

--- ui/containers/DeliveryList/index.js
+++ ui/containers/DeliveryList/index.js
@@ -20,7 +20,7 @@ import options from './options'
 import { formatNumber } from '~/ui/shared/utils'
 
 
-@connect(state=>({
+@connect(state => ({
     place: state.place,
     order: orderSelectors.getOrder(state),
     session: authSelectors.getSession(state),
@@ -31,76 +31,107 @@ export default class extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {          
-          selectedPlace: props.place.listPlace.map(item => item.placeId).join(','),
-          refreshing: false,
-          loading: false,
-        }  
+        this.state = {
+            selectedPlace: props.place.listPlace.map(item => item.placeId).join(','),
+            refreshing: false,
+            loading: false,
+        }
 
         this.selectedStatus = 0
     }
 
-    componentWillFocus(){        
+    componentWillFocus() {
 
         // make it like before    
-        const {order} = this.props
-        if(!order.orderList.length) {
-          this.loadPage()
-        } 
+        const { order } = this.props
+        let dateFilter = this.refs.dateFilter.getData(); //currentSelectValue
+        // if(!order.orderList.length) {
+        this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
+        // } 
 
         this.setState({
-          refreshing: false,
-        })
-        
-    }
-
-    componentWillMount(){
-        this.componentWillFocus()      
-    }
-
-    loadPage(page=1){
-       const {session, getOrderList} = this.props      
-       const {selectedPlace} = this.state
-        getOrderList(session, selectedPlace, this.selectedStatus, page, 
-          ()=>this.setState({
             refreshing: false,
-            loading: false,
-          }))
+        })
+
     }
 
-    _handleChangePlace = (item) => {        
+    // componentWillMount(){
+    //     this.componentWillFocus()      
+    // }
+    componentDidMount() {
+        this.componentWillFocus()
+    }
+
+
+    loadPage(page = 1, from_time, to_time) {
+        const { session, getOrderList } = this.props
+        const { selectedPlace } = this.state
+        getOrderList(session, selectedPlace, this.selectedStatus, page,
+            from_time, to_time,
+            () => this.setState({
+                refreshing: false,
+                loading: false,
+            }))
+    }
+
+    _handleChangePlace = (item) => {
+        let dateFilter = this.refs.dateFilter.getData() //currentSelectValue      
         this.setState({
-          selectedPlace: item.id,
-        }, ()=>this.loadPage())                
+            selectedPlace: item.id,
+        }, () => this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to))
     }
 
-    _onRefresh =() => {    
-      this.setState({refreshing: true})        
-      this.loadPage()
-    }    
-
-    _loadMore = ()=>{
-      if(this.state.loading || this.state.refreshing)
-        return
-      // console.log('load more')
-      const {order} = this.props
-      if(order.hasMore){
-        this.setState({loading: true})          
-        this.loadPage(order.page + 1)              
-      }        
+    _onRefresh = () => {
+        let dateFilter = this.refs.dateFilter.getData()
+        this.setState({ refreshing: true })
+        this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
     }
-        
+
+    _loadMore = () => {
+        if (this.state.loading || this.state.refreshing)
+            return
+        const { order } = this.props
+        let dateFilter = this.refs.dateFilter.getData()
+        if (order.hasMore) {
+            this.setState({ loading: true })
+            this.loadPage(order.page + 1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
+        }
+    }
+
     _handlePressTab = (item) => {
         this.selectedStatus = item.tabID
-        this.loadPage()
+        let dateFilter = this.refs.dateFilter.getData()
+        this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
     }
-
-    _renderRow({orderInfo, orderRowList}) {
-        var statusBlock=null;
+    _handlePressFilter = (item) => {
+        this.loadPage(1, item.currentSelectValue.value.from, item.currentSelectValue.value.to)
+    }
+    _renderRow({ orderInfo, orderRowList }) {
+        var statusBlock = null;
         // var bottomBlock=null;
         const status = this.selectedStatus
-        if (status === 0){
-            statusBlock =  (
+        var orderListBock = null
+        if (orderRowList != null) {
+            orderListBock =(
+                <View>
+                    <View style={styles.block}>
+                        {orderRowList.map((subItem, index) =>
+                            (
+                                <View key={index} style={styles.row}>
+                                    <Text bold>{subItem.itemName}</Text>
+                                    <Text>SL: <Text bold>{subItem.quantity}</Text></Text>
+                                </View>
+                            )
+                        )}
+                    </View>
+                    <Border color='rgba(0,0,0,0.5)' size={1} />
+                </View>
+            )
+        }
+
+
+        if (status === 0) {
+            statusBlock = (
                 <View style={styles.deliveryCodeBlock}>
                     <Icon name='order-history' style={{ ...styles.deliveryCodeWaitingConfirm, ...styles.icon }} />
                     <Text style={styles.deliveryCodeWaitingConfirm}>{orderInfo.orderCode}</Text>
@@ -112,7 +143,7 @@ export default class extends Component {
             //         <Button transparent><Text style={styles.confirm}>Xác nhận</Text></Button>
             //     </View>
             // )
-        }else if(status === 1){
+        } else if (status === 1) {
             statusBlock = (
                 <View style={styles.deliveryCodeBlock}>
                     <Icon name='shiping-bike2' style={{ ...styles.deliveryCodeWaitingDelivery, ...styles.icon }} />
@@ -125,7 +156,7 @@ export default class extends Component {
             //         <Button transparent><Text style={styles.confirm}>Đã giao thành công</Text></Button>
             //     </View>
             // )
-        }else {
+        } else {
             statusBlock = (
                 <View style={styles.deliveryCodeBlock}>
                     <Icon name='done' style={{ ...styles.deliveryCodeSuccess, ...styles.icon }} />
@@ -142,17 +173,7 @@ export default class extends Component {
                     </View>
                 </View>
                 <Border color='rgba(0,0,0,0.5)' size={1} />
-                <View style={styles.block}>
-                    {orderRowList.map((subItem, index)=>
-                        (
-                            <View key={index} style={styles.row}>
-                                <Text bold>{subItem.itemName}</Text>
-                                <Text>SL: <Text bold>{subItem.quantity}</Text></Text>
-                            </View>
-                        )
-                    )}
-                </View>
-                <Border color='rgba(0,0,0,0.5)' size={1} />
+                {orderListBock}
                 <View style={styles.block}>
                     <Text>
                         <Text style={{ fontWeight: 'bold' }}>Ghi chú: </Text>
@@ -179,14 +200,14 @@ export default class extends Component {
                         <Text bold>{formatNumber(Math.round(orderInfo.moneyAmount))}đ</Text>
                     </View>
                 </View>
-                
+
             </View>
         )
     }
 
     render() {
         const { handleSubmit, submitting, place } = this.props
-        
+
         let defaultSelected = {
             id: this.state.selectedPlace,
             name: "Tất cả địa điểm"
@@ -197,24 +218,25 @@ export default class extends Component {
         }))
         dropdownValues = [defaultSelected, ...dropdownValues]
 
-        const {orderList} = this.props.order
-        
+        const { orderList } = this.props.order
+
 
         return (
             <Container style={styles.container}>
                 <TopDropdown dropdownValues={dropdownValues} onSelect={this._handleChangePlace} selectedOption={defaultSelected} />
-                
-                <TabsWithNoti tabData={options.tabData} 
-                  activeTab={0} onPressTab={this._handlePressTab} />
 
-                <Content 
-                onEndReached={this._loadMore} onRefresh={this._onRefresh}             
-                refreshing={this.state.refreshing} 
-                style={styles.contentContainer}>
-                        {orderList && orderList.map(item=>(
-                            this._renderRow(item)
-                        ))}
-                    
+                <TabsWithNoti tabData={options.tabData}
+                    activeTab={0} onPressTab={this._handlePressTab} />
+                <DateFilter onPressFilter={this._handlePressFilter} ref='dateFilter' />
+
+                <Content
+                    onEndReached={this._loadMore} onRefresh={this._onRefresh}
+                    refreshing={this.state.refreshing}
+                    style={styles.contentContainer}>
+                    {orderList && orderList.map(item => (
+                        this._renderRow(item)
+                    ))}
+
 
                 </Content>
             </Container>
