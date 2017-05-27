b53b139ad027756694bfe47229ad55b4caebddef
tienvm58 <tienvm_58@vnu.edu.vn>
2017-05-27 14:48:10 +0700

    update const color

--- ui/shared/constants.js
+++ ui/shared/constants.js
@@ -1,8 +1,20 @@
-export const PRIMARY_COLOR = '#00a9d4'
-export const WARNING_COLOR = 'orange'
-export const ERROR_COLOR = 'red'
-export const SUCCESS_COLOR = 'green'
-export const BACKGROUND_COLOR_1 = '#F2F2F2'
-export const TEXT_COLOR_1 = '#828282'
+export const DARK_BLUE = '#0085AC'
+export const LIGHT_BLUE = '#00A9D3'
+export const LIGHT_GREEN = '#8CC63F'
+export const ORANGE = '#F7931E'
+export const DARK_GRAY = '#4D4D4D'
+export const GRAY = '#808080'
+export const LIGHT_GRAY = '#B3B3B3'
+export const BRIGHT_GRAY = '#E8E8E8'
+export const SUPER_BRIGHT_GRAY = '#F2F2F2'
+export const RED = 'red'
+
+export const PRIMARY_COLOR = LIGHT_BLUE
+export const WARNING_COLOR = ORANGE
+export const ERROR_COLOR = RED
+export const SUCCESS_COLOR = LIGHT_GREEN
+export const BACKGROUND_COLOR_1 = BRIGHT_GRAY
+export const TEXT_COLOR_1 = GRAY
+
 export const BASE_COUNTDOWN_ORDER_MINUTE = 45
 export const BASE_COUNTDOWN_BOOKING_MINUTE = 30
