01bda6f6dc72085a6b32b9ed3948900073356735
Thanh Tu <tubackkhoa@gmail.com>
2017-05-29 12:17:12 +0700

    update notification

--- store/constants/api.js
+++ store/constants/api.js
@@ -7,3 +7,10 @@ export const API_BASE = 'http://dev.clingme.net:9099'
 export const CLINGME_SERVER = 'http://dev.clingme.net:9099/'
 export const SENDER_ID = '1075989080862'
 export const API_TIMEOUT = 60000
+
+
+export const NOTIFY_TYPE = {
+  WAITING: 5,
+  BOOKING: 6,
+  SUCCESS: 7,
+}
