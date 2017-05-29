dbb94d02f003a33beb1488ad8b880cb408b8a4d7
hai.vu <vu.longhai93@gmail.com>
2017-05-29 15:52:20 +0700

    Fix delivery, placeOrerDetail

--- ui/containers/DeliveryList/index.js
+++ ui/containers/DeliveryList/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container, List, ListItem, Text, Thumbnail, Button, Tabs, Tab, TabHeading, ScrollableTab, Input, Radio, Card } from 'native-base'
-import { View, TouchableWithoutFeedback, Animated, Picker, Easing, TextInput, Modal, TouchableOpacity } from 'react-native'
+import { Container, List, ListItem, Text, Thumbnail, Button, Spinner } from 'native-base'
+import { View, Modal } from 'react-native'
 import { Field, reduxForm } from 'redux-form'
 import styles from './styles'
 import TopDropdown from '~/ui/components/TopDropdown'
@@ -38,8 +38,8 @@ export default class extends Component {
 
         this.state = {
             selectedPlace: props.place.listPlace.map(item => item.placeId).join(','),
-            refreshing: false,
             loading: false,
+            loadingMore: false,
             modalOpen: false,
             phoneNumber: ''
         }
@@ -47,22 +47,12 @@ export default class extends Component {
         this.selectedStatus = 0
         this.interval = 0
     }
-    _timerCallback = (beforeTime) => {
-        console.log('Call _timerCallback', beforeTime)
-        const now = (new Date()).getTime()
-        console.log('Now', now)
-        console.log('Now -Before', now - beforeTime)
-        if (now - beforeTime > 2000) {
-            console.log('Over 2 second')
-            requestAnimationFrame(this._timerCallback)
-        }
 
-    }
     componentWillFocus() {
         const { order } = this.props
         let dateFilter = this.refs.dateFilter.getData(); //currentSelectValue
         this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
-        this.setState({ refreshing: false, counting: true })
+        this.setState({counting: true })
     }
 
     componentDidMount() {
@@ -70,18 +60,23 @@ export default class extends Component {
     }
 
     componentWillBlur() {
-        console.log('Delivery Will Blur')
         this.setState({ counting: false })
     }
 
-    loadPage(page = 1, from_time, to_time) {
-        const { session, getOrderList } = this.props
+    loadPage(page = 1, from_time, to_time, isLoadMore=false) {
+        const { session, getOrderList, clearOrderList } = this.props
         const { selectedPlace } = this.state
+        if (isLoadMore){
+            this.setState({loadingMore: true})
+        }else{
+            clearOrderList()
+            this.setState({loading: true})
+        }
         getOrderList(session, selectedPlace, this.selectedStatus, page,
             from_time, to_time,
             () => this.setState({
-                refreshing: false,
                 loading: false,
+                loadingMore: false,
             }))
     }
 
@@ -99,33 +94,32 @@ export default class extends Component {
     }
 
     _handleChangePlace = (item) => {
-        let dateFilter = this.refs.dateFilter.getData() //currentSelectValue      
+        let dateFilter = this.refs.dateFilter.getData().currentSelectValue.value //currentSelectValue      
         this.setState({
             selectedPlace: item.id,
-        }, () => this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to))
+        }, () => this.loadPage(1, dateFilter.from, dateFilter.to))
     }
 
     _onRefresh = () => {
         let dateFilter = this.refs.dateFilter.getData()
-        this.setState({ refreshing: true })
         this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
     }
 
     _loadMore = () => {
-        if (this.state.loading || this.state.refreshing)
+        if (this.state.loading || this.state.loadingMore)
             return
         const { order } = this.props
-        let dateFilter = this.refs.dateFilter.getData()
+        console.log('Has More', order)
+        let dateFilter = this.refs.dateFilter.getData().currentSelectValue.value
         if (order.hasMore) {
-            this.setState({ loading: true })
-            this.loadPage(order.page + 1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
+            this.loadPage(order.page + 1, dateFilter.from, dateFilter.to, true)
         }
     }
 
     _handlePressTab = (item) => {
         this.selectedStatus = item.tabID
-        let dateFilter = this.refs.dateFilter.getData()
-        this.loadPage(1, dateFilter.currentSelectValue.value.from, dateFilter.currentSelectValue.value.to)
+        let dateFilter = this.refs.dateFilter.getData().currentSelectValue.value
+        this.loadPage(1, dateFilter.from, dateFilter.to)
     }
     _handlePressFilter = (item) => {
         this.loadPage(1, item.currentSelectValue.value.from, item.currentSelectValue.value.to)
@@ -168,17 +162,27 @@ export default class extends Component {
                     <Text style={styles.deliveryCodeWaitingDelivery}>{orderInfo.orderCode}</Text>
                 </View>
             )
-        } else {
+        } else if (status == ORDER_SUCCESS){
             statusBlock = (
                 <View style={styles.deliveryCodeBlock}>
-                    <Icon name='done' style={{ ...styles.deliveryCodeSuccess, ...styles.icon }} />
+                    {/*<Icon name='done' style={{ ...styles.deliveryCodeSuccess, ...styles.icon }} />*/}
+                    <Icon name='shiping-bike2' style={{ ...styles.deliveryCodeSuccess, ...styles.icon }} />
                     <Text style={styles.deliveryCodeSuccess}>{orderInfo.orderCode}</Text>
+                </View>
+            )
+        }else{
+            statusBlock = (
+                <View style={styles.deliveryCodeBlock}>
+                    {/*<Icon name='done' style={{ ...styles.deliveryCodeSuccess, ...styles.icon }} />*/}
+                    <Icon name='shiping-bike2' style={{ ...styles.grey, ...styles.icon }} />
+                    <Text style={styles.grey}>{orderInfo.orderCode}</Text>
                 </View>
             )
         }
         const countTo = orderInfo.clingmeCreatedTime + BASE_COUNTDOWN_ORDER_MINUTE * 60
+        let listItemStyle = (status!=ORDER_CANCEL) ? styles.deliveryBlock:styles.deliveryBlockCacel
         return (
-            <ListItem style={styles.deliveryBlock} key={orderInfo.clingmeId}
+            <ListItem style= {listItemStyle} key={orderInfo.clingmeId}
                 onPress={() => {
                     console.log('Forwarding: ', orderInfo.orderCode)
                     forwardTo('deliveryDetail/' + orderInfo.orderCode)
@@ -189,6 +193,9 @@ export default class extends Component {
                         {statusBlock}
                         <View style={styles.row}>
                             <Text style={styles.time}>{moment(orderInfo.clingmeCreatedTime * 1000).format('hh:mm:ss   DD/MM/YYYY')}</Text>
+                            {(status == ORDER_SUCCESS) && (
+                                <Icon name='done' style={{ ...styles.deliveryCodeSuccess, ...styles.icon }} />
+                            )}
                             <CircleCountdown baseMinute={BASE_COUNTDOWN_ORDER_MINUTE}
                                 counting={this.state.counting}
                                 countTo={countTo}
@@ -198,26 +205,25 @@ export default class extends Component {
                 </View>
                 <Border color='rgba(0,0,0,0.5)' size={1} />
                 {orderListBock}
-                <View style={styles.block}>
-                    <View style={{ ...styles.row, width: '100%', paddingLeft: 10, paddingRight: 10 }}>
-                        <Text style={{ fontWeight: 'bold' }}>Ghi chú: </Text>
-                        <Text>{orderInfo.note}</Text>
-                    </View>
+                <View>
+                    <View style={styles.rowLeft}><Text bold style={styles.textLeft}>Ghi chú: </Text></View>
+                    <View style={styles.rowLeft}><Text style={styles.textLeft}>{orderInfo.note}</Text></View>
                 </View>
                 <Border color='rgba(0,0,0,0.5)' size={1} />
                 <View style={styles.block}>
-                    <View style={{ ...styles.row, marginBottom: 5 }}>
-                        <View style={styles.row}>
-                            <Icon name='account' style={styles.icon} />
-                            <Text>{orderInfo.userInfo.memberName}</Text>
-                        </View>
-                        <View style={styles.row}>
-                            <Icon name='phone' style={{ ...styles.phoneIcon, ...styles.icon }} />
-                            <Text
-                                onPress={this.onModalOpen.bind(this, orderInfo.userInfo.phoneNumber)}
-                                style={styles.phoneNumber}>{formatPhoneNumber(orderInfo.userInfo.phoneNumber)}</Text>
-                        </View>
-                    </View>
+                    {orderInfo.userInfo &&
+                        (<View style={{ ...styles.row, marginBottom: 5 }}>
+                            <View style={styles.row}>
+                                <Icon name='account' style={styles.icon} />
+                                <Text>{orderInfo.userInfo.memberName}</Text>
+                            </View>
+                            <View style={styles.row}>
+                                <Icon name='phone' style={{ ...styles.phoneIcon, ...styles.icon }} />
+                                <Text
+                                    onPress={this.onModalOpen.bind(this, orderInfo.userInfo.phoneNumber)}
+                                    style={styles.phoneNumber}>{formatPhoneNumber(orderInfo.userInfo.phoneNumber)}</Text>
+                            </View>
+                        </View>)}
 
                     <View style={{ ...styles.row, marginBottom: 5 }}>
                         <Text>Địa chỉ: {orderInfo.placeInfo.address}</Text>
@@ -250,7 +256,7 @@ export default class extends Component {
 
 
         const { orderList } = this.props.order
-
+        console.log('Order List', orderList)
 
         return (
             <Container style={styles.container}>
@@ -266,12 +272,15 @@ export default class extends Component {
                 <Content
                     contentContainerStyle={styles.contentContainerStyle}
                     onEndReached={this._loadMore} onRefresh={this._onRefresh}
-                    refreshing={this.state.refreshing}
+                    refreshing={this.state.loading}
                     style={styles.contentContainer}
                 >
                     {orderList && orderList.map(item => (
                         this._renderRow(item)
                     ))}
+                    {this.state.loadingMore &&
+                        <Spinner />
+                    }
                 </Content>
             </Container>
         )
