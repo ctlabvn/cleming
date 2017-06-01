a9ed31269dd65d0ea9cbb055b5e8727c711ea446
tienvm58 <tienvm_58@vnu.edu.vn>
2017-06-01 09:56:43 +0700

    disable all colors code were defined in file

--- ui/shared/constants.js
+++ ui/shared/constants.js
@@ -2,24 +2,24 @@
  *  Try not to use it anymore
  */
 
-
-export const BLUE600 = '#0085AC'
-export const BLUE400 = '#00A9D3'
-export const GREEN400 = '#8CC63F'
-export const ORANGE500 = '#F7931E'
-export const GRAY600 = '#4D4D4D'
-export const GRAY500 = '#808080'
-export const GRAY400 = '#B3B3B3'
-export const GRAY300 = '#E8E8E8'
-export const GRAY200 = '#F2F2F2'
-export const RED500 = 'red'
-
-export const PRIMARY_COLOR = BLUE400
-export const WARNING_COLOR = ORANGE500
-export const ERROR_COLOR = RED500
-export const SUCCESS_COLOR = GREEN400
-export const BACKGROUND_COLOR_1 = GRAY300
-export const TEXT_COLOR_1 = GRAY500
+//
+// export const BLUE600 = '#0085AC'
+// export const BLUE400 = '#00A9D3'
+// export const GREEN400 = '#8CC63F'
+// export const ORANGE500 = '#F7931E'
+// export const GRAY600 = '#4D4D4D'
+// export const GRAY500 = '#808080'
+// export const GRAY400 = '#B3B3B3'
+// export const GRAY300 = '#E8E8E8'
+// export const GRAY200 = '#F2F2F2'
+// export const RED500 = 'red'
+//
+// export const PRIMARY_COLOR = BLUE400
+// export const WARNING_COLOR = ORANGE500
+// export const ERROR_COLOR = RED500
+// export const SUCCESS_COLOR = GREEN400
+// export const BACKGROUND_COLOR_1 = GRAY300
+// export const TEXT_COLOR_1 = GRAY500
 
 export const BASE_COUNTDOWN_ORDER_MINUTE = 45
 export const BASE_COUNTDOWN_BOOKING_MINUTE = 30
