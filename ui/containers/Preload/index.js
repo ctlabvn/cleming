1cc8e34ce506dbf4bd96c667b3f98f38acdc1c95
Thanh Tu <tubackkhoa@gmail.com>
2017-06-08 15:57:43 +0700

    revert

--- ui/containers/Preload/index.js
+++ ui/containers/Preload/index.js
@@ -1,37 +1,61 @@
 import React, { Component } from 'react'
+import { Animated } from 'react-native'
 import { Container, Spinner, Text } from 'native-base'
 import material from '~/theme/variables/material'
 import styles from './styles'
 
-// export default class extends Component {
+export default class extends Component {
   
-//   render(){
-//     const {message='Please waiting...'} = this.props
-//     return (
-//       <Container style={styles.container}>
-//         <Text>{message}</Text>
-//         <Spinner color={material.tabBarActiveTextColor} />
-//       </Container>
-//     )
+  render(){
+    const {message='Please waiting...'} = this.props
+    return (
+      <Container style={styles.container}>
+        <Text>{message}</Text>
+        <Spinner color={material.tabBarActiveTextColor} />
+      </Container>
+    )
+  }
+}
+
+// import Svg,{
+//     LinearGradient,
+//     Rect,
+//     Defs,
+//     Stop
+// } from 'react-native-svg'
+
+// export default class extends Component {
+
+//   constructor(props) {
+//      super(props);
+//      this.state = {
+//        pan: new Animated.Value(0), // inits to zero
+//      };
+//    }
+
+//   componentDidMount(){
+//     Animated.spring(
+//        this.state.pan,         // Auto-multiplexed
+//        {toValue: 1} // Back to zero
+//      ).start();
 //   }
+
+//   render(){
+
+//     console.log(this.state.pan)
+//     return (
+
+//       <Svg style={styles.container}>
+//           <Defs>
+//               <LinearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0">            
+//                   <Stop offset="0%" stopColor="#ff0000" stopOpacity="1"/>
+//                   <Stop offset="100%" stopColor="#ffffff" stopOpacity="1"/>              
+//               </LinearGradient>
+//           </Defs>          
+//           <Rect width="100%" height="100%" fill="url(#grad)"/>
+//       </Svg>
+//     )
+
+//   }
+
 // }
-
-import Svg,{
-    LinearGradient,
-    Rect,
-    Defs,
-    Stop
-} from 'react-native-svg'
-
-export default ({style, ...props}) => (
-
-  <Svg {...props} style={styles.container}>
-      <Defs>
-          <LinearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0">            
-              <Stop offset="0%" stopColor="#ff0000" stopOpacity="1"/>
-              <Stop offset="100%" stopColor="#ffffff" stopOpacity="1"/>              
-          </LinearGradient>
-      </Defs>          
-      <Rect width="100%" height="100%" fill="url(#grad)"/>
-  </Svg>
-)
