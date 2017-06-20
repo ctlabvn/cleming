a4ce747d657506b47abee5e91285013b13fc997c
hai.vu <vu.longhai93@gmail.com>
2017-06-20 17:30:36 +0700

    +1 zoonmLevel

--- ui/containers/Report/index.js
+++ ui/containers/Report/index.js
@@ -40,6 +40,8 @@ export default class Report extends Component {
         }
         this.isLoadingPlace = false
         this.showMap = false
+        this.mapWidth = 0
+        this.mapHeight = 0
     }
     _requestMapData(placeIds, fromTime, toTime, callback) {
         const { xsession, getMapReport } = this.props
@@ -48,15 +50,18 @@ export default class Report extends Component {
         let maxLa = this.state.region.latitude + this.state.region.latitudeDelta
         let maxLo = this.state.region.longitude + this.state.region.longitudeDelta
         const { height, width } = Dimensions.get('window')
-
         const bounds = [
             minLo,
             minLa,
             maxLo,
             maxLa
         ]
-        let zoomLevel = geoViewport.viewport(bounds, [height, width]).zoom
-        console.log('Geo View Port', zoomLevel)
+        // let zoomLevel = geoViewport.viewport(bounds, [height, width]).zoom
+        // console.log('Geo View Port', zoomLevel)
+        // console.log('Map Width Height', this.mapWidth+'---'+this.mapHeight)
+        let zoomLevel = geoViewport.viewport(bounds, [this.mapHeight, this.mapWidth]).zoom
+        zoomLevel += 1
+        console.log('New Geo Viewport', zoomLevel)
         if (callback) {
             getMapReport(xsession, placeIds, minLa, minLo, maxLa, maxLo, zoomLevel, fromTime, toTime, callback)
         } else {
@@ -81,21 +86,9 @@ export default class Report extends Component {
             }
         )
     }
-    // componentWillReceiveProps(nextProps) {
-    //     if (this.isLoadingPlace && nextProps.place && nextProps.place.listPlace) {
-    //         this.isLoadingPlace = false
-
-    //         let selectedPlace = {}
-    //         selectedPlace.id = nextProps.place.listPlace[0].placeId
-    //         selectedPlace.name = nextProps.place.listPlace[0].address
-
-    //         let dateFilterData = this.refs.dateFilter.getData().currentSelectValue.value
-    //         this._loadAndFocus(selectedPlace.id, dateFilterData.from, dateFilterData.to)
-    //     }
-    // }
     componentDidMount() {
         InteractionManager.runAfterInteractions(() => {
-            const {app} = this.props
+            const { app } = this.props
             app.topDropdown.setCallbackPlaceChange(this._handleTopDropdown)
             let dateFilterData = this.refs.dateFilter.getData().currentSelectValue.value
             let selectedPlace = app.topDropdown.getValue()
@@ -104,16 +97,16 @@ export default class Report extends Component {
                 this.isLoadingPlace = true
                 return
             }
-            this._loadAndFocus(selectedPlace.id, dateFilterData.from, dateFilterData.to)            
+            setTimeout(() => {
+                this._loadAndFocus(selectedPlace.id, dateFilterData.from, dateFilterData.to)
+            }, 500)
+
         })
-
-
-
     }
 
     componentWillFocus() {
         InteractionManager.runAfterInteractions(() => {
-            const {app} = this.props
+            const { app } = this.props
             app.topDropdown.setCallbackPlaceChange(this._handleTopDropdown)
             let dateFilterData = this.refs.dateFilter.getData().currentSelectValue.value
             let selectedPlace = app.topDropdown.getValue()
@@ -121,14 +114,15 @@ export default class Report extends Component {
                 this.isLoadingPlace = true
                 return
             }
-            this._loadAndFocus(selectedPlace.id, dateFilterData.from, dateFilterData.to)
+            setTimeout(()=>{
+                this._loadAndFocus(selectedPlace.id, dateFilterData.from, dateFilterData.to)
+            }, 500)
         })
-
     }
     _regionChange = (region) => {
         this.setState({ region },
             () => {
-                const {app} = this.props
+                const { app } = this.props
                 let placeData = app.topDropdown.getValue()
                 let dateFilterData = this.refs.dateFilter.getData().currentSelectValue.value
                 this._requestMapData(placeData.id, dateFilterData.from, dateFilterData.to)
@@ -162,9 +156,14 @@ export default class Report extends Component {
             }
         )
     }
+    _handleOnLayoutMap = e => {
+        const { width, height } = e.nativeEvent.layout
+        this.mapWidth = width
+        this.mapHeight = height
+    }
 
     _handlePressFilter = (item) => {
-        const {app} = this.props
+        const { app } = this.props
         let placeData = app.topDropdown.getValue()
         let dateFilterData = item.currentSelectValue.value
         this._requestMapData(placeData.id, dateFilterData.from, dateFilterData.to)
@@ -173,168 +172,7 @@ export default class Report extends Component {
     _sum(arr) {
         return arr.reduce((x1, x2) => x1 + x2, 0)
     }
-    _renderCustomerStatistic() {
-        const { report } = this.props
-        if (!report || !report.customer) {
-            return <Text>Loading statistic...</Text>
-        }
-        const userInfo = report.customer.userInfo
-        const baseLength = 250
 
-        //Caculate gender visual
-        let femaleRate = 0.5
-        if (userInfo.total != 0) {
-            femaleRate = parseInt(userInfo.femaleNumber) / (parseInt(userInfo.femaleNumber) + parseInt(userInfo.maleNumber))
-        }
-        const femalePercent = Math.floor(femaleRate * 100)
-        const malePercent = 100 - femalePercent
-        const femaleBarLength = Math.floor(femaleRate * baseLength)
-        const maleBarLength = baseLength - femaleBarLength
-
-
-        // Caculate IncomeLevel visual
-        const totalIncomeLevel = this._sum(userInfo.incomeLevels)
-        let incomeLevel1Rate = 0.33, incomeLevel2Rate = 0.33
-        if (totalIncomeLevel != 0) {
-            incomeLevel1Rate = userInfo.incomeLevels[0] / totalIncomeLevel
-            incomeLevel2Rate = userInfo.incomeLevels[1] / totalIncomeLevel
-        }
-        const incomeLevel1Percent = Math.floor(incomeLevel1Rate * 100)
-        const incomeLevel2Percent = Math.floor(incomeLevel2Rate * 100)
-        const incomeLevel3Percent = 100 - incomeLevel1Percent - incomeLevel2Percent
-        const incomeLevel1Length = Math.floor(incomeLevel1Rate * baseLength)
-        const incomeLevel2Length = Math.floor(incomeLevel2Rate * baseLength)
-        const incomeLevel3Length = baseLength - incomeLevel1Length - incomeLevel2Length
-
-
-        // Caculate distance visual
-        const totalDistanceLevel = this._sum(userInfo.distanceLevels)
-        let distanceLevel1Rate = 0.33, distanceLevel2Rate = 0.33
-        if (totalDistanceLevel != 0) {
-            distanceLevel1Rate = userInfo.distanceLevels[0] / totalDistanceLevel
-            distanceLevel2Rate = userInfo.distanceLevels[1] / totalDistanceLevel
-        }
-        const distanceLevel1Percent = Math.floor(distanceLevel1Rate * 100)
-        const distanceLevel2Percent = Math.floor(distanceLevel2Rate * 100)
-        const distanceLevel3Percent = 100 - distanceLevel1Percent - distanceLevel2Percent
-        const distanceLevel1Length = Math.floor(distanceLevel1Rate * baseLength)
-        const distanceLevel2Length = Math.floor(distanceLevel2Rate * baseLength)
-        const distanceLevel3Length = baseLength - distanceLevel1Length - distanceLevel2Length
-
-        // Caculate visual view time linbe
-        let viewFrom16to17Length
-        let viewFrom8to10Length
-        let viewRemainLength
-        let viewFrom16to17Percent
-        let viewFrom8to10Percent
-        let viewRemainPercent
-        let viewFrom16to17
-        let viewFrom8to10
-        let viewRemain
-        let totalTimeView = userInfo.timeView ? this._sum(userInfo.timeView) : 0
-        if (totalTimeView == 0) {
-            viewFrom16to17Length = 0.33 * baseLength
-            viewFrom8to10Length = 0.33 * baseLength
-            viewRemainLength = baseLength - viewFrom16to17Length - viewFrom8to10Length
-            viewFrom16to17Percent = 33
-            viewFrom8to10Percent = 33
-            viewRemainPercent = 33
-        } else {
-            viewFrom16to17 = this._sum(userInfo.timeView.slice(16, 18))
-            viewFrom8to10 = this._sum(userInfo.timeView.slice(8, 11))
-            viewRemain = this._sum(userInfo.timeView) - viewFrom16to17 - viewFrom8to10
-            viewFrom16to17Length = Math.floor(viewFrom16to17 / totalTimeView * baseLength)
-            viewFrom8to10Length = Math.floor(viewFrom8to10 / totalTimeView * baseLength)
-            viewRemainLength = baseLength - viewFrom16to17Length - viewFrom8to10Length
-            viewFrom16to17Percent = Math.floor(viewFrom16to17 / totalTimeView * 100)
-            viewFrom8to10Percent = Math.floor(viewFrom8to10 / totalTimeView * 100)
-            viewRemainPercent = 100 - viewFrom16to17Length - viewFrom8to10Percent
-        }
-
-
-
-        return (
-            <View style={styles.graphBlock}>
-                <View style={styles.graphRow}>
-                    <Text small style={styles.graphLabel}>Khách hàng</Text>
-                    <View style={styles.graphDraw}>
-                        <View>
-                            <View style={styles.row}>
-                                <Text small style={{ ...styles.textCenter, width: femaleBarLength }}>{femalePercent}% Nữ</Text>
-                                <Text small style={{ ...styles.textCenter, width: maleBarLength }}>{malePercent}% Nam</Text>
-                            </View>
-                            <View style={styles.graphDraw}>
-                                <View style={{ ...styles.bar, ...styles.backgroundBlue, width: femaleBarLength }}></View>
-                                <View style={{ ...styles.bar, ...styles.backgroundPrimary, width: maleBarLength }}></View>
-                            </View>
-                        </View>
-
-                    </View>
-                </View>
-                <View style={styles.graphRow}>
-                    <Text small style={styles.graphLabel}>Chi tiêu</Text>
-                    <View>
-                        <View style={styles.row}>
-                            <Text small style={{ ...styles.textCenterBottom, width: incomeLevel1Length }}>{incomeLevel1Percent}%</Text>
-                            <Text small style={{ ...styles.textCenterBottom, width: incomeLevel2Length }}>{incomeLevel2Percent}%</Text>
-                            <Text small style={{ ...styles.textCenterBottom, width: incomeLevel3Length }}>{incomeLevel3Percent}%</Text>
-                        </View>
-                        <View style={styles.graphDraw}>
-                            <View style={{ ...styles.bar, ...styles.backgroundBlue, width: incomeLevel1Length }}></View>
-                            <View style={{ ...styles.bar, ...styles.backgroundPrimary, width: incomeLevel2Length }}></View>
-                            <View style={{ ...styles.bar, ...styles.backgroundGrey, width: incomeLevel3Length }}></View>
-                        </View>
-                        <View style={styles.row}>
-                            <Text small primary style={{ ...styles.rightText, width: incomeLevel1Length }}>&lt;2tr</Text>
-                            <Text small primary style={{ ...styles.rightText, width: incomeLevel2Length }}>&lt;5tr</Text>
-                            <Text small transparent style={{ ...styles.rightText, width: incomeLevel3Length }}>''</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.graphRow}>
-                    <Text small style={styles.graphLabel}>Thời gian xem KM</Text>
-                    <View>
-                        <View style={styles.row}>
-                            <Text small style={{ ...styles.textCenter, width: viewFrom16to17Length }}>{viewFrom16to17Percent}%</Text>
-                            <Text small style={{ ...styles.textCenter, width: viewFrom8to10Length }}>{viewFrom8to10Percent}%</Text>
-                            <Text small style={{ ...styles.textCenter, width: viewRemainLength }}>{viewRemainPercent}%</Text>
-                        </View>
-                        <View style={styles.graphDraw}>
-                            <View style={{ ...styles.bar, ...styles.backgroundBlue, width: viewFrom16to17Length }}></View>
-                            <View style={{ ...styles.bar, ...styles.backgroundPrimary, width: viewFrom8to10Length }}></View>
-                            <View style={{ ...styles.bar, ...styles.backgroundGrey, width: viewRemainLength }}></View>
-                        </View>
-                        <View style={styles.row}>
-                            <Text small primary style={{ ...styles.textCenter, width: viewFrom16to17Length }}>16h-17h</Text>
-                            <Text small primary style={{ ...styles.textCenter, width: viewFrom8to10Length }}>8h-10h</Text>
-                            <Text small transparent style={{ ...styles.textCenter, width: viewRemainLength }}>''</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.graphRow}>
-                    <Text small style={styles.graphLabel}>Khoảng cách</Text>
-                    <View>
-                        <View style={styles.row}>
-                            <Text small style={{ ...styles.textCenter, width: distanceLevel1Length }}>{distanceLevel1Percent}%</Text>
-                            <Text small style={{ ...styles.textCenter, width: distanceLevel2Length }}>{distanceLevel2Percent}%</Text>
-                            <Text small style={{ ...styles.textCenter, width: distanceLevel3Length }}>{distanceLevel3Percent}%</Text>
-                        </View>
-                        <View style={styles.graphDraw}>
-                            <View style={{ ...styles.bar, ...styles.backgroundBlue, width: distanceLevel1Length }}></View>
-                            <View style={{ ...styles.bar, ...styles.backgroundPrimary, width: distanceLevel2Length }}></View>
-                            <View style={{ ...styles.bar, ...styles.backgroundGrey, width: distanceLevel3Length }}></View>
-                        </View>
-                        <View style={styles.row}>
-                            <Text small primary style={{ ...styles.rightText, width: distanceLevel1Length }}>&lt;2km</Text>
-                            <Text small primary style={{ ...styles.rightText, width: distanceLevel2Length }}>&lt;   5km</Text>
-                            <Text small transparent style={{ ...styles.rightText, width: distanceLevel3Length }}></Text>
-                        </View>
-                    </View>
-
-                </View>
-            </View>
-        )
-    }
     render() {
         const { report, place } = this.props
         // let dropdownValues = place.listPlace.map(item => ({
@@ -344,7 +182,7 @@ export default class Report extends Component {
 
         return (
             <Container style={styles.container}>
-                <View style={{height: '100%'}} >
+                <View style={{ height: '100%' }} >
                     <DateFilter onPressFilter={this._handlePressFilter} ref='dateFilter' defaultFilter='week' type='lite' />
                     <MapView
                         region={this.state.region}
@@ -352,7 +190,8 @@ export default class Report extends Component {
                         style={{ width: '100%', height: '100%' }}
                         onRegionChangeComplete={this.onRegionChangeComplete}
                         moveOnMarkerPress={false}
-                        >
+                        onLayout={this._handleOnLayoutMap}
+                    >
                         {report && report.map && report.map.locationDtos.map((marker, idx) => {
                             return (
                                 <MapView.Marker key={idx}
@@ -376,7 +215,7 @@ export default class Report extends Component {
 
                         })}
                     </MapView>
-                    
+
                 </View>
             </Container>
         )
