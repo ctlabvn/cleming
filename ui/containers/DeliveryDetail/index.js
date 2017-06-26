1fba70b9825e3e85f4949663d5c31f8534375b54
hai.vu <vu.longhai93@gmail.com>
2017-06-26 18:35:47 +0700

    Update order detail

--- ui/containers/DeliveryDetail/index.js
+++ ui/containers/DeliveryDetail/index.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Button, Container, List, ListItem, Spinner, Text } from "native-base";
-import { Image, InteractionManager, View, TouchableWithoutFeedback } from "react-native";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Button, Container, List, ListItem, Spinner, Text} from "native-base";
+import {Image, InteractionManager, View, TouchableWithoutFeedback} from "react-native";
 import styles from "./styles";
 import * as orderActions from "~/store/actions/order";
 import * as commonActions from "~/store/actions/common";
 import * as orderSelectors from "~/store/selectors/order";
 import * as notificationActions from "~/store/actions/notification";
 import * as authSelectors from "~/store/selectors/auth";
-import { InputField } from "~/ui/elements/Form";
+import {InputField} from "~/ui/elements/Form";
 import Content from "~/ui/components/Content";
-import { formatNumber, formatPhoneNumber } from "~/ui/shared/utils";
+import {formatNumber, formatPhoneNumber} from "~/ui/shared/utils";
 import moment from "moment";
 import CircleCountdown from "~/ui/components/CircleCountdown";
-import { BASE_COUNTDOWN_ORDER_MINUTE } from "~/ui/shared/constants";
-import { DEFAULT_TIME_FORMAT, DELIVERY_FEEDBACK, FAST_DELIVERY, GENERAL_ERROR_MESSAGE } from "~/store/constants/app";
+import {BASE_COUNTDOWN_ORDER_MINUTE} from "~/ui/shared/constants";
+import {DEFAULT_TIME_FORMAT, DELIVERY_FEEDBACK, FAST_DELIVERY, GENERAL_ERROR_MESSAGE} from "~/store/constants/app";
 import material from "~/theme/variables/material.js";
 import DeliveryFeedbackDialog from "~/ui/containers/DeliveryList/DeliveryFeedbackDialog";
 import Icon from "~/ui/elements/Icon"
@@ -22,7 +22,7 @@ import CallModal from "~/ui/components/CallModal"
 @connect(state => ({
     xsession: authSelectors.getSession(state),
     order: orderSelectors.getOrder(state),
-}), { ...orderActions, ...commonActions, ...notificationActions })
+}), {...orderActions, ...commonActions, ...notificationActions})
 
 export default class extends Component {
     constructor(props) {
@@ -35,10 +35,11 @@ export default class extends Component {
             phoneNumber: ''
         }
     }
+
     _load() {
-        const { route, getOrderDetail, xsession, setToast, forwardTo, updateRead, order, getOrderDenyReason } = this.props
+        const {route, getOrderDetail, xsession, setToast, forwardTo, updateRead, order, getOrderDenyReason} = this.props
         let deliveryId = route.params.id
-        this.setState({ loading: true })
+        this.setState({loading: true})
         getOrderDetail(xsession, deliveryId,
             (err, data) => {
                 if (err) {
@@ -52,7 +53,7 @@ export default class extends Component {
                     return
                 }
                 if (data && data.updated) {
-                    this.setState({ orderDetail: data.updated, loading: false })
+                    this.setState({orderDetail: data.updated, loading: false})
                     if (data.updated.orderInfo && !data.updated.orderInfo.isReadCorrespond
                         && data.updated.orderInfo.notifyIdCorrespond) {
                         updateRead(xsession, data.updated.orderInfo.notifyIdCorrespond)
@@ -68,25 +69,28 @@ export default class extends Component {
 
     componentDidMount() {
         // InteractionManager.runAfterInteractions(() => {
-            const { app } = this.props
-            this._load()
+        const {app} = this.props
+        this._load()
         // })
     }
+
     componentWillFocus() {
         // InteractionManager.runAfterInteractions(() => {
-            console.log('Will focus DeliveryDetail')
-            const { app } = this.props
-            this.setState({ counting: true })
-            this._load()
+        console.log('Will focus DeliveryDetail')
+        const {app} = this.props
+        this.setState({counting: true})
+        this._load()
         // })
     }
+
     componentWillBlur() {
         // InteractionManager.runAfterInteractions(() => {
-            this.setState({ counting: false })
+        this.setState({counting: false})
         // })
     }
+
     _handleFeedbackOrder = (posOrderId, reasonId, note) => {
-        const { updateOrderStatus, setToast, xsession, markWillReload, forwardTo } = this.props
+        const {updateOrderStatus, setToast, xsession, markWillReload, forwardTo} = this.props
         updateOrderStatus(xsession, posOrderId, DELIVERY_FEEDBACK.CANCEL, reasonId, note,
             (err, data) => {
                 if (data && data.updated && data.updated.data && data.updated.data.success) {
@@ -99,7 +103,7 @@ export default class extends Component {
         )
     }
     _handleConfirmOrder = (posOrderId) => {
-        const { updateOrderStatus, setToast, xsession, markWillReload, forwardTo } = this.props
+        const {updateOrderStatus, setToast, xsession, markWillReload, forwardTo} = this.props
         updateOrderStatus(xsession, posOrderId, DELIVERY_FEEDBACK.OK,
             (err, data) => {
                 if (data && data.updated && data.updated.data && data.updated.data.success) {
@@ -118,6 +122,7 @@ export default class extends Component {
     _onRefresh = () => {
         this._load()
     }
+
     _renderStatusText(status) {
         switch (status) {
             case 'WAIT_CONFIRM':
@@ -131,6 +136,7 @@ export default class extends Component {
                 return <Text gray small bold>Đã hủy</Text>
         }
     }
+
     onModalOpen = (phoneNumber) => {
         this.setState({
             modalOpen: true,
@@ -142,12 +148,19 @@ export default class extends Component {
             modalOpen: false
         })
     }
+
     render() {
         console.log('Render DeliveryDetail')
-        const { route, order } = this.props
+        const {route, order} = this.props
         if (!this.state || !this.state.orderDetail || Object.keys(this.state.orderDetail).length == 0) {
             return (
-                <View style={{ backgroundColor: material.white500, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <View style={{
+                    backgroundColor: material.white500,
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100%'
+                }}>
                     <Spinner />
                     <Text small>Loading...</Text>
                 </View>
@@ -168,9 +181,10 @@ export default class extends Component {
                 </View>
                 <View style={styles.rowPadding}>
                     <Text small grayDark>Phí giao hàng:</Text>
-                    <Text bold grayDark>{orderDetail.shipPriceReal > 0 ? formatNumber(orderDetail.shipPriceReal) : 0}đ</Text>
+                    <Text bold
+                          grayDark>{orderDetail.shipPriceReal > 0 ? formatNumber(orderDetail.shipPriceReal) : 0}đ</Text>
                 </View>
-                <View style={styles.line} />
+                <View style={styles.line}/>
                 <View style={styles.rowPadding}>
                     <Text small grayDark>Tổng tiền thanh toán: </Text>
                     <Text bold error>{formatNumber(orderDetail.orderInfo.moneyAmount)}đ</Text>
@@ -178,17 +192,27 @@ export default class extends Component {
             </View>
         )
         let containerStyle = (orderDetail.orderInfo.status == 'CONFIRMED') ? styles.container2 : styles.container
+        let rejectReason = null
+        if (orderDetail && orderDetail.orderInfo && orderDetail.orderInfo.orderRejectReason
+            && (orderDetail.orderInfo.orderRejectReason.reason || orderDetail.orderInfo.orderRejectReason.note)){
+            rejectReason = (orderDetail.orderInfo.orderRejectReason.reason || orderDetail.orderInfo.orderRejectReason.note)
+        }
+        console.log('Reject Reason', rejectReason)
         return (
             <Container style={containerStyle}>
                 <DeliveryFeedbackDialog ref='deliveryFeedbackDialog'
-                    listValue={order.denyReason}
-                    onClickYes={this._handleFeedbackOrder}
+                                        listValue={order.denyReason}
+                                        onClickYes={this._handleFeedbackOrder}
                 />
                 <CallModal
                     phoneNumber={this.state.phoneNumber}
                     onCloseClick={this.onModalClose.bind(this)}
-                    open={this.state.modalOpen} />
-                <View style={{ ...styles.rowPadding, ...styles.backgroundPrimary, width: '100%', justifyContent: 'center' }}>
+                    open={this.state.modalOpen}/>
+                <View style={{
+                    ...styles.rowPadding, ...styles.backgroundPrimary,
+                    width: '100%',
+                    justifyContent: 'center'
+                }}>
                     <Text white center bold>{orderDetail.orderInfo.placeInfo.address}</Text>
                 </View>
 
@@ -196,25 +220,34 @@ export default class extends Component {
                     <View style={styles.rowPadding}>
                         {this._renderStatusText(orderDetail.orderInfo.status)}
                         <View style={styles.row}>
-                            <Text small grayDark style={{ marginRight: 5 }}>{moment(orderDetail.orderInfo.clingmeCreatedTime * 1000).format(DEFAULT_TIME_FORMAT)}</Text>
+                            <Text small grayDark
+                                  style={{marginRight: 5}}>{moment(orderDetail.orderInfo.clingmeCreatedTime * 1000).format(DEFAULT_TIME_FORMAT)}</Text>
                             {(orderDetail.orderInfo.enableFastDelivery == FAST_DELIVERY.YES) &&
-                                <CircleCountdown
-                                    baseMinute={BASE_COUNTDOWN_ORDER_MINUTE}
-                                    counting={this.state.counting}
-                                    countTo={countTo}
-                                />}
+                            <CircleCountdown
+                                baseMinute={BASE_COUNTDOWN_ORDER_MINUTE}
+                                counting={this.state.counting}
+                                countTo={countTo}
+                            />}
                         </View>
                     </View>
                     <View style={styles.rowPadding}>
                         <Text small grayDark>Đặt hàng số</Text>
                         <Text primary bold>{orderDetail.orderInfo.tranId}</Text>
                     </View>
-                    <View style={{ ...styles.block, ...styles.paddingTopMedium }}>
-                        <Text small grayDark>Phản hồi của khách hàng</Text>
-                        <Text bold grayDark>{orderDetail.orderInfo.feedback}</Text>
-                    </View>
-                    <View style={styles.line} />
-                    <View style={{ ...styles.block, paddingBottom: 0 }}>
+                    {(orderDetail.orderInfo.feedback && orderDetail.orderInfo.feedback!="")&&
+                        <View style={{...styles.block, ...styles.paddingTopMedium}}>
+                            <Text small grayDark>Phản hồi của khách hàng</Text>
+                            <Text bold grayDark>{orderDetail.orderInfo.feedback}</Text>
+                        </View>
+                    }
+                    {(rejectReason)&&
+                        <View style={{...styles.block, ...styles.paddingTopMedium}}>
+                            <Text small grayDark>Lí do hủy đơn hàng</Text>
+                            <Text bold grayDark>{rejectReason}</Text>
+                        </View>
+                    }
+                    <View style={styles.line}/>
+                    <View style={{...styles.block, paddingBottom: 0}}>
                         <Text small grayDark>Địa chỉ giao hàng</Text>
                         <Text bold grayDark>{orderDetail.orderInfo.fullAddress}</Text>
                     </View>
@@ -232,8 +265,9 @@ export default class extends Component {
                             }}>
                             <View style={styles.row}>
 
-                                <Icon name='phone' style={{ ...styles.icon, ...styles.phoneIcon }} />
-                                <Text bold primary>{formatPhoneNumber(orderDetail.orderInfo.userInfo.phoneNumber)}</Text>
+                                <Icon name='phone' style={{...styles.icon, ...styles.phoneIcon}}/>
+                                <Text bold
+                                      primary>{formatPhoneNumber(orderDetail.orderInfo.userInfo.phoneNumber)}</Text>
 
                             </View>
                         </TouchableWithoutFeedback>
@@ -246,28 +280,28 @@ export default class extends Component {
                         </View>
                     }
 
-                    <View style={{ ...styles.block, ...styles.paddingTopMedium }}>
+                    <View style={{...styles.block, ...styles.paddingTopMedium}}>
                         <Text small grayDark>Yêu cầu khác</Text>
                         <Text bold grayDark>{orderDetail.orderInfo.note}</Text>
                     </View>
-                    <View style={styles.line} />
+                    <View style={styles.line}/>
                     <View style={styles.rowPadding}>
                         <Text small bold grayDark>Giỏ hàng: {totalItem}</Text>
                     </View>
                     <List dataArray={orderDetail.orderRowList}
-                        renderRow={(item) => (
-                            <ListItem style={styles.orderItem}>
-                                <View style={styles.cartLeft}>
-                                    <Image style={{ width: 60, height: 60 }} source={{ uri: item.itemImage }} />
-                                    <View style={styles.cartContent}>
-                                        <Text small grayDark style={styles.textLeftFlex}>{item.itemName}</Text>
-                                        <Text small grayDark style={styles.textLeft}>Số lượng: {item.quantity}</Text>
-                                    </View>
-                                </View>
-                                <Text bold grayDark style={{ ...styles.itemCash }}>{item.price / 1000}k</Text>
-                            </ListItem>
-                        )
-                        }>
+                          renderRow={(item) => (
+                              <ListItem style={styles.orderItem}>
+                                  <View style={styles.cartLeft}>
+                                      <Image style={{width: 60, height: 60}} source={{uri: item.itemImage}}/>
+                                      <View style={styles.cartContent}>
+                                          <Text small grayDark style={styles.textLeftFlex}>{item.itemName}</Text>
+                                          <Text small grayDark style={styles.textLeft}>Số lượng: {item.quantity}</Text>
+                                      </View>
+                                  </View>
+                                  <Text bold grayDark style={{...styles.itemCash}}>{item.price / 1000}k</Text>
+                              </ListItem>
+                          )
+                          }>
                     </List>
                     {(orderDetail.orderInfo.status == 'CONFIRMED') && moneyBlock}
                 </Content>
@@ -279,11 +313,11 @@ export default class extends Component {
 
                 {(orderDetail.orderInfo.status == 'CONFIRMED') && (
                     <View style={styles.fixButtonBlock}>
-                        <Button style={{ ...styles.buttonFeedback, ...styles.backgroundLightGray }}
-                            onPress={() => this.showReasonPopup(orderDetail.orderInfo.clingmeId)}
+                        <Button style={{...styles.buttonFeedback, ...styles.backgroundLightGray}}
+                                onPress={() => this.showReasonPopup(orderDetail.orderInfo.clingmeId)}
                         ><Text gray>Hủy giao hàng</Text></Button>
-                        <Button style={{ ...styles.buttonFeedback, ...styles.backgroundPrimary }}
-                            onPress={() => this._handleConfirmOrder(orderDetail.orderInfo.clingmeId)}
+                        <Button style={{...styles.buttonFeedback, ...styles.backgroundPrimary}}
+                                onPress={() => this._handleConfirmOrder(orderDetail.orderInfo.clingmeId)}
                         ><Text white>Đã giao hàng</Text></Button>
                     </View>
                 )}
