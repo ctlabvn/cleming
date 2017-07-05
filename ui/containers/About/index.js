38c17235b4e3cfab4350a7a019492f9be15b00d8
tienvm58 <tienvm_58@vnu.edu.vn>
2017-07-05 17:56:27 +0700

    update About screen

--- ui/containers/About/index.js
+++ ui/containers/About/index.js
@@ -1,15 +1,50 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import styles from './styles'
-import { View, Text } from 'react-native'
+import {View, Text, ScrollView} from 'react-native'
+
+import Icon from '~/ui/elements/Icon'
+
+import {connect} from 'react-redux'
+import * as commonActions from '~/store/actions/common'
+
+@connect(null, commonActions)
 
 export default class extends Component {
+
     render() {
         return (
-            <View>
-                <Text>
-                    this is infromation about this application
-                </Text>
+            <View style={styles.container}>
+                <ScrollView>
+                    <Icon name="logo" style={styles.logoIcon}/>
+                    <Text style={styles.textTitle}>
+                        Giới thiệu
+                    </Text>
+                    <Text style={styles.textContent}>
+                        Ứng dụng "Đối tác Clingme" giúp cho các nhà bán hàng trên Clingme theo dõi các giao dịch của
+                        khách
+                        hàng với cửa hàng của mình thông qua chiếc smartphone của mình.
+                        Các nhà bán hàng có thể kiểm tra nhanh khách hàng tiềm năng, hiệu quả bán hàng, doanh thu cũng
+                        như
+                        tình trạng các đơn hàng.
+                    </Text>
+                    <Text style={styles.textTitle}>
+                        Phiên bản
+                    </Text>
+                    <Text style={styles.textContent}>
+                        1.x
+                    </Text>
+                    <Text style={styles.textTitle}>
+                        Liên hệ
+                    </Text>
+                    <Text style={styles.textContent}>
+                        support@gigatum.com
+                    </Text>
+                    <Text style={styles.textContent}>
+                        Clingme được thành lập từ 2013 với mục tiêu đem đến cho các nhà bán hàng kênh bán hàng hiệu quả
+                        và nhanh chóng
+                    </Text>
+                </ScrollView>
             </View>
         )
     }
--- ui/containers/About/styles.js
+++ ui/containers/About/styles.js
@@ -1,5 +1,31 @@
+import material from '~/theme/variables/material'
+
 export default {
     container: {
         flex: 1,
-    }
+        padding: 15,
+    },
+
+    logoIcon:{
+        color: material.red500,
+        fontSize: 60,
+        alignSelf: 'center',
+        marginTop: 25,
+    },
+
+    textTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: material.gray600,
+        marginTop: 35,
+    },
+
+    textContent: {
+        marginTop: 10,
+        marginBottom: 5,
+        color: material.gray600,
+        fontWeight: 'normal',
+        fontSize: 15,
+    },
+
 }
