ace84fcf60d1b2c12090853325a3d0b3729f7387
tubackkhoa <tubackkhoa@gmail.com>
2017-07-07 18:27:01 +0700

    fix api base

--- store/constants/api.js
+++ store/constants/api.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 const LOCAL_IP = Platform.OS === 'ios' ? '127.0.0.1' : '10.0.3.2'
 
 // export const API_BASE = __DEV__ ? 'http://dev.clingme.net:9099' : 'http://sale.clingme.vn:9868'
-export const API_BASE =  'http://dev.clingme.net:9099' 
+export const API_BASE =  'http://sale.clingme.vn:9868' 
 export const CLINGME_SERVER = 'http://dev.clingme.net:9099/'
 export const SENDER_ID = '1075989080862'
 export const API_TIMEOUT = 10000
