7634ca0c6f35adcc84d4c7661849c9dc88f40065
tubackkhoa <tubackkhoa@gmail.com>
2017-07-08 16:28:01 +0700

    fix map and fix android project

--- android/app/src/main/java/com/gigatum/merchantapp/MainApplication.java
+++ android/app/src/main/java/com/gigatum/merchantapp/MainApplication.java
@@ -6,7 +6,6 @@ import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
 import com.smixx.fabric.FabricPackage;
 import com.reactnative.photoview.PhotoViewPackage;
-import com.burlap.filetransfer.FileTransferPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.gigatum.merchantapp.BuildConfig;
@@ -36,7 +35,6 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new FabricPackage(),
             new PhotoViewPackage(),
-            new FileTransferPackage(),
             new RNDeviceInfo(),
             new ReactNativePushNotificationPackage(),
             new RNFSPackage(),
