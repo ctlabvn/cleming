ca9106ba2a6d15c9d527d34599d166994de98498
tubackkhoa <tubackkhoa@gmail.com>
2017-07-26 16:25:45 +0700

    added test automation

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,35 +1,13 @@
 import 'react-native';
 import React from 'react';
-import URL from 'url-parse'
-// import Index from '../index.ios.js';
+// import Index from '../index.android.js';
 
 // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
-import { matchPath } from 'react-router'
-const routes = {
-  'abc': false,  
-  'userManagement/action/createUser':true,
-  'userManagement/action':true,
-  'def': false,
-}
-
-const matchRoutes = (pathname) => {
-  let match = null
-  for(route in routes) {
-    match = matchPath(pathname, {
-      path:route,
-      exact: true,
-      strict: false,
-    })
-    if(match) 
-      break    
-  }
-  return match
-}
 
 it('renders correctly', () => {
-  const match = matchRoutes('userManagement/action/createUser')
-  // const url = new URL('delegation/detail/refresh?key=value', null, true)
-  // console.log(url)
-  console.log(match)
+  // const tree = renderer.create(
+  //   <Index />
+  // );
 });
+
