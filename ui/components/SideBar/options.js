df4fcc5b34df3306009ceec6911e453baff733e7
Hai Vu <vu.longhai93@gmail.com>
2017-08-29 15:03:34 +0700

    Hide feature Cashout, Checking, update TransDetail

--- ui/components/SideBar/options.js
+++ ui/components/SideBar/options.js
@@ -19,24 +19,24 @@ export default {
             route: 'comingSoon',
             icon: 'post',
         },
-        {
-            name: I18n.t('checking'),
-            route: 'checking',
-            icon: 'term',
-            masterOnly: true
-        },
-
         // {
-        //     name: I18n.t('revenue'),
-        //     route: 'revenueManagementList',
+        //     name: I18n.t('checking'),
+        //     route: 'checking',
         //     icon: 'term',
+        //     masterOnly: true
         // },
-        {
-            name: I18n.t('page_cashout_account'),
-            route: 'cashoutAccount',
-            icon: 'cash_out',
-            masterOnly: true
-        },
+
+        // // {
+        // //     name: I18n.t('revenue'),
+        // //     route: 'revenueManagementList',
+        // //     icon: 'term',
+        // // },
+        // {
+        //     name: I18n.t('page_cashout_account'),
+        //     route: 'cashoutAccount',
+        //     icon: 'cash_out',
+        //     masterOnly: true
+        // },
 
         {
             name: I18n.t('manage_account'),
