b27d09b1d3e55a9f3d0287114cf8e671b0fb5ba6
tienvm58 <tienvm_58@vnu.edu.vn>
2017-09-28 18:16:13 +0700

    doing issue #667

--- ui/containers/CashoutDetail/index.js
+++ ui/containers/CashoutDetail/index.js
@@ -31,7 +31,42 @@ export default class CashoutHistory extends Component {
     render() {
         return (
             <Container style={styles.container}>
-                <Text medium bold>Chi tiết Rút tiền</Text>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 10}}>
+                    <Text medium bold grayDark>Chi tiết Rút tiền</Text>
+                    <Text medium bold orange>999.999.999 đ</Text>
+                </View>
+
+                <Text style={{margin: 10}}>Thông tin chi tiết</Text>
+
+                <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+                    <Icon name="account" style={{fontSize: 50}}/>
+                    <View style={{marginLeft: 10}}>
+                        <Text>Ngân hàng TMCP VN</Text>
+                        <Text>*1234</Text>
+                    </View>
+                </View>
+
+                <View style={{marginTop: 10, marginLeft: 10}}>
+                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
+                        <Text medium bold>Tên chủ tài khoản</Text>
+                        <Text medium style={{marginRight: 10}}>Chu Thanh Phong</Text>
+                    </View>
+
+                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
+                        <Text medium bold>Mã giao dịch</Text>
+                        <Text>99999999</Text>
+                    </View>
+
+                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
+                        <Text medium bold>Thời gian yêu cầu</Text>
+                        <Text>17:30 06/09/1969</Text>
+                    </View>
+
+                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
+                        <Text medium bold>Thời gian nhận tiền</Text>
+                        <Text>17:30 06/09/1969</Text>
+                    </View>
+                </View>
             </Container>
         )
 
