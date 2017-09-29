7d04aad2a35f4ecf30ce04a5ff84faf3c34cb25b
tienvm58 <tienvm_58@vnu.edu.vn>
2017-09-29 09:38:28 +0700

    doing issue #667 CashoutDetail screen: init view 100%

--- ui/containers/CashoutDetail/index.js
+++ ui/containers/CashoutDetail/index.js
@@ -31,40 +31,47 @@ export default class CashoutHistory extends Component {
     render() {
         return (
             <Container style={styles.container}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 10}}>
-                    <Text medium bold grayDark>Chi tiết Rút tiền</Text>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 15}}>
+                    <Text medium bold grayDark>Số tiền rút</Text>
                     <Text medium bold orange>999.999.999 đ</Text>
                 </View>
 
-                <Text style={{margin: 10}}>Thông tin chi tiết</Text>
+                <View style={{backgroundColor: material.gray300}}>
+                    <Text medium italic bold grayDark style={{margin: 15}}>Thông tin chi tiết</Text>
+                </View>
 
-                <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
-                    <Icon name="account" style={{fontSize: 50}}/>
-                    <View style={{marginLeft: 10}}>
-                        <Text>Ngân hàng TMCP VN</Text>
-                        <Text>*1234</Text>
+                <View style={{flexDirection: 'row', margin: 15, alignItems: 'center'}}>
+                    <Icon gray name="account" style={{fontSize: 50}}/>
+                    <View style={{marginLeft: 15}}>
+                        <Text medium bold grayDark>Ngân hàng TMCP VN</Text>
+                        <Text style={{marginTop: 5}}>*1234</Text>
                     </View>
                 </View>
 
-                <View style={{marginTop: 10, marginLeft: 10}}>
-                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
-                        <Text medium bold>Tên chủ tài khoản</Text>
-                        <Text medium style={{marginRight: 10}}>Chu Thanh Phong</Text>
+                <View style={styles.detailContainer}>
+                    <Border/>
+                    <View style={styles.rowDetail}>
+                        <Text medium bold grayDark>Tên chủ tài khoản</Text>
+                        <Text medium grayDark style={styles.rowSubDetail}>Chu Thanh Phong</Text>
                     </View>
 
-                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
-                        <Text medium bold>Mã giao dịch</Text>
-                        <Text>99999999</Text>
+                    <Border/>
+                    <View style={styles.rowDetail}>
+
+                        <Text medium bold grayDark>Mã giao dịch</Text>
+                        <Text medium grayDark style={styles.rowSubDetail}>99999999</Text>
                     </View>
 
-                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
-                        <Text medium bold>Thời gian yêu cầu</Text>
-                        <Text>17:30 06/09/1969</Text>
+                    <Border/>
+                    <View style={styles.rowDetail}>
+                        <Text medium bold grayDark>Thời gian yêu cầu</Text>
+                        <Text medium grayDark style={styles.rowSubDetail}>17:30 06/09/1969</Text>
                     </View>
 
-                    <View style={{flexDirection: 'row', borderTopWidth: 1, justifyContent: 'space-between'}}>
-                        <Text medium bold>Thời gian nhận tiền</Text>
-                        <Text>17:30 06/09/1969</Text>
+                    <Border/>
+                    <View style={styles.rowDetail}>
+                        <Text medium bold grayDark>Thời gian nhận tiền</Text>
+                        <Text medium grayDark style={styles.rowSubDetail}>17:30 06/09/1969</Text>
                     </View>
                 </View>
             </Container>
--- ui/containers/CashoutDetail/styles.js
+++ ui/containers/CashoutDetail/styles.js
@@ -3,63 +3,24 @@ export default {
 	container: {
 		backgroundColor: 'white'
 	},
+
 	row: {
 		flexDirection: 'row',
+	},
+	detailContainer: {
+		marginTop: 15,
+		marginLeft: 15
+	},
+	rowDetail: {
+		flexDirection: 'row',
+		// borderTopWidth: 1,
 		justifyContent: 'space-between',
-		alignItems: 'center',
+		paddingVertical: 15,
 	},
-	rowPadding: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		padding: 10
-	},
-	rowPaddingLarge: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		paddingTop: 20,
-		paddingBottom: 20,
-		paddingLeft: 10,
-		paddingRight: 10
-	},
-	listItem: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		padding: 10,
-		marginLeft: 25,
-		borderBottomWidth: 1,
-		borderColor: material.gray300
-	},
-	rowCenter: {
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-		padding: 10
-	},
-	borderTop: {
-		borderTopWidth: 1,
-		borderColor: material.gray300
-	},
-	borderBottom: {
-		borderBottomWidth: 1,
-		borderColor: material.gray300
-	},
-	rowLeft: {
-		flexDirection: 'row',
-		justifyContent: 'flex-start',
-		alignItems: 'center',
-		padding: 10
-	},
-	forwardIconWarning: {
-		fontSize: 20,
-		color: material.warningColor
-	},
-	forwardIconSuccess: {
-		fontSize: 20,
-		color: material.successColor
+	rowSubDetail: {
+		marginRight: 15
 	}
+
 
 
 }
