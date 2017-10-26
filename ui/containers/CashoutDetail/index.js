99e45351d4b9765ddc1a347c0e203c4992d3e69f
tienvm58 <tienvm_58@vnu.edu.vn>
2017-10-26 15:58:15 +0700

    clean code of Cashout Detail screen

--- ui/containers/CashoutDetail/index.js
+++ ui/containers/CashoutDetail/index.js
@@ -1,21 +1,17 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Button, Container, Text} from "native-base";
-import {InteractionManager, View, TouchableWithoutFeedback, ActivityIndicator, Image} from "react-native";
+import {View, Image} from "react-native";
 import styles from "./styles";
 import DateFilter from "~/ui/components/DateFilter";
 import * as commonAction from "~/store/actions/common";
 import * as walletAction from "~/store/actions/wallet";
-import TabsWithNoti from "~/ui/components/TabsWithNoti";
-import Icon from "~/ui/elements/Icon";
 import Border from "~/ui/elements/Border";
 import moment from "moment";
 import {formatNumber, chainParse} from "~/ui/shared/utils";
-import Content from "~/ui/components/Content";
 import {getSession} from "~/store/selectors/auth";
 import material from "~/theme/variables/material.js";
 import I18n from '~/ui/I18n'
-import ListViewExtend from '~/ui/components/ListViewExtend'
 import Spinner from '~/ui/components/Spinner'
 import {
     DEFAULT_TIME_FORMAT
